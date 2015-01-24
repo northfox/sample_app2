bfd29b09114974df358255ec998532c132381517
test <test@example.com>
2015-01-24 15:31:45 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,15 +3,6 @@ require "spec_helper"
 describe "Static pages" do
   subject { page }
 
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
-  
   describe "Home page" do
     before { visit root_path }
 
